4ce57cd0adabb72fcdbdd1eee825fef49979bf0b
badplye-minelist <57844182+badplye-minelist@users.noreply.github.com>
2019-11-17 10:27:26 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,23 @@
 const Discord = require('discord.js');
 
 const client = new Discord.Client();
+const exampleEmbed = new Discord.RichEmbed()
+	.setColor('#0099ff')
+	.setTitle('Some title')
+	.setURL('https://discord.js.org/')
+	.setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
+	.setDescription('Some description here')
+	.setThumbnail('https://i.imgur.com/wSTFkRM.png')
+	.addField('Regular field title', 'Some value here')
+	.addBlankField()
+	.addField('Inline field title', 'Some value here', true)
+	.addField('Inline field title', 'Some value here', true)
+	.addField('Inline field title', 'Some value here', true)
+	.setImage('https://i.imgur.com/wSTFkRM.png')
+	.setTimestamp()
+	.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
 
+channel.send(helpembed);
  
 
 client.on('ready', () => { //On successful login
@@ -21,9 +37,8 @@ client.on('message', message => {
 client.on('message', message => {
 
     if (message.content === '?embed') {
-
-       message.reply('_embed #channel author=name= url=https://mine-list.com | title=Help | description=this is a help page for minelist | field=name=Test value=test | field=name=test1 value=test1 | color=#4f545c');
-
+     
+     channel.send({ embed: helpembed });
        }
 
 });
