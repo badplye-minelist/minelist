4da7a4ccc43a67684020e8dfe0e8b212038ae93e
badplye-minelist <57844182+badplye-minelist@users.noreply.github.com>
2019-11-16 21:37:40 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,12 +6,12 @@ const client = new Discord.Client();
 
 client.on('ready', () => { //On successful login
     console.log("Up and running!");
-    client.user.setPresence({ game: { name: 'mine-list.com | !help', type: 0 } });
+    client.user.setPresence({ game: { name: 'mine-list.com | ?help', type: 0 } });
 });
 
 client.on('message', message => {
 
-    if (message.content === '!ping') {
+    if (message.content === '?ping') {
 
        message.reply('pong');
 
@@ -22,9 +22,9 @@ client.on('message', message => {
 
 client.on('message', message => {
 
-    if (message.content === '!help') {
+    if (message.content === '?help') {
 
-       message.reply('The commands I offer are:\n\n**!help** | Shows a list of commands.\n**!website** | Gives a website link.\n**!suppport** | Lists easiest ways to get support.\n**!about** | Learn more about Minelist!\n**!ping** | Pong ;).');
+       message.reply('The commands I offer are:\n\n**?help** | Shows a list of commands.\n**?website** | Gives a website link.\n**?suppport** | Lists easiest ways to get support.\n**?about** | Learn more about Minelist!\n**?ping** | Pong ;).');
 
        }
 
@@ -32,7 +32,7 @@ client.on('message', message => {
 
 client.on('message', message => {
 
-    if (message.content === '!support') {
+    if (message.content === '?support') {
 
        message.reply('Need some help? Submit a ticket at https://mine-list.com/support/\n\nAlternatively, you can reach out to an admin via __admin@mine-list.com__!');
 
@@ -42,7 +42,7 @@ client.on('message', message => {
 
 client.on('message', message => {
 
-    if (message.content === '!about') {
+    if (message.content === '?about') {
 
        message.reply('Founded by Jordan, Julius, and Aiden, Minelist operates with a goal to provide users with an unbiased expirence. Created from the ground up, hours of work and effort have been put in to making Minelist your site!');
 
@@ -53,7 +53,7 @@ client.on('message', message => {
 
 client.on('message', message => {
 
-    if (message.content === '!website') {
+    if (message.content === '?website') {
 
        message.reply('The website is: https://mine-list.com/');
 
